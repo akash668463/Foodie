ce211c92733224f2b02269ce0885b24b67387cf1
Akash <ak668463@gmail.com>
2026-03-06 00:20:23 +0530

    Request, Response Dto and Base Service Interface and Implementation

---
+++ Foodie/Foodie.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using Foodie.Web.Models;
+
+namespace Foodie.Web.Service.IService
+{
+    public interface IBaseService
+    {
+            Task<ResponseDto> SendAsync(RequestDto requestDto);
+    }
+}
---
+++ Foodie/Foodie.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Foodie.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
