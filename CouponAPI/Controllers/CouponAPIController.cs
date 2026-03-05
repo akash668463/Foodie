6ad1d9bd0b2fc4f3104f6aca662b35d80b158f38
Akash <ak668463@gmail.com>
2026-03-06 02:48:48 +0530

    Creating CouponController and also views for it

--- CouponAPI/Controllers/CouponAPIController.cs
+++ CouponAPI/Controllers/CouponAPIController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CouponAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/coupon")]
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
@@ -114,6 +114,7 @@ namespace CouponAPI.Controllers
         }
 
         [HttpDelete]
+        [Route("{id:int}")]
         public ResponseDto Delete(int id)
         {
             try
