05e0c4c01313d9c48005f094485263bbb5491888
Akash <ak668463@gmail.com>
2026-03-05 21:08:10 +0530

    Creating CouponAPI

---
+++ CouponAPI/MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CouponAPI.Models;
+using CouponAPI.Models.Dto;
+
+namespace CouponAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Coupon, CouponDto>();
+            CreateMap<CouponDto, Coupon>();
+        }
+    }
+}
--- CouponAPI/Program.cs
+++ CouponAPI/Program.cs
@@ -1,6 +1,17 @@
+using AutoMapper;
+using CouponAPI;
+using CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register AutoMapper
+builder.Services.AddAutoMapper(typeof(Program));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,5 +32,19 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+ApplyMigration();
 app.Run();
+
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
