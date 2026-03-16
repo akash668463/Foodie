72dacc142ebaeaa142ef30f1df8a5036363d98b4
Akash <ak668463@gmail.com>
2026-03-16 23:19:24 +0530

    ShoppingCartAPI Added

---
+++ Foodie/ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ShoppingCartAPI.Models;
+using ShoppingCartAPI.Models.Dtos;
+
+namespace ShoppingCartAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+            CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+        }
+    }
+}
---
+++ Foodie/ShoppingCartAPI/Models/Dtos/CartDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingCartAPI.Models.Dtos
+{
+    public class CartDto
+    {
+        public CartHeaderDto CartHeader { get; set; }
+        public IEnumerable<CartDetailsDto>? CartDetails { get; set; }
+    }
+}
