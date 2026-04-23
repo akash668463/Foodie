c58ef930ad5d2aaf5078229564fdbd30384e6a9c
Akash <ak668463@gmail.com>
2026-04-23 14:38:35 +0530

    Image Upload Changes

--- Foodie/Foodie.Web/Service/IService/IOrderService.cs
+++ Foodie/Foodie.Web/Service/IService/IOrderService.cs
@@ -8,5 +8,9 @@ namespace Foodie.Web.Service.IService
         Task<ResponseDto?> CreateStripeSession(StripeRequestDto stripeRequestDto);
 
         Task<ResponseDto?> ValidateStripeSession(int orderHeaderId);
+
+        Task<ResponseDto?> GetAllOrder(string? userId);
+        Task<ResponseDto?> GetOrder(int orderId);
+        Task<ResponseDto?> UpdateOrderStatus(int orderId, string newStatus);
     }
 }
